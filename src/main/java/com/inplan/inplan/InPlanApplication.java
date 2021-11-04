f163f4bf4a14085149b368f50cf507d5acf15017
Yun-SeYeong <dbstpdud09@naver.com>
2021-11-04 22:28:40 +0900

    [UPDATE] init spring project

---
+++ src/main/java/com/inplan/inplan/InPlanApplication.java
@@ -0,0 +1,13 @@
+package com.inplan.inplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InPlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inplan/inplan/InPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inplan.inplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
